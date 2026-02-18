3d86396d752824bf8ddee35ccb97b4b29fc2176c
Almar Klein <almar@almarklein.org>
2026-02-18 12:23:26 +0100

    Improve and simplify logic related to labels (#11)

--- test_version.py
+++ test_version.py
@@ -7,15 +7,56 @@ import sys
 
 import _version
 
+example_git_output = [
+    (("", "", "", ""), "0.0.0"),
+    (("", "", "unknown", ""), "0.0.0"),
+    (("", "", "unknown", "dirty"), "0.0.0+unknown.dirty"),
+    (("0.0.0", "", "abcd", ""), "0.0.0"),
+    (("0.0.0", "", "abcd", "dirty"), "0.0.0+abcd.dirty"),
+    (("0.0.0", "1", "abcd", ""), "0.0.0.post1+abcd"),
+    (("0.0.0", "1", "abcd", "dirty"), "0.0.0.post1+abcd.dirty"),
+]
+
+
+def test_get_extended_version() -> None:
+    """Test get_extended_version function."""
+    for args, ref in example_git_output:
+        v = _version.get_extended_version(*args)
+        assert v == ref, f"{args} -> {v} != {ref}"
+
+
+def test_failsafe() -> None:
+    """Test that in case anything errors, we don't crash."""
+    ori_repo_dir = _version.repo_dir
+    del _version.repo_dir
+
+    try:
+        v = _version.get_version()
+        assert v == "0.0.0"
+
+    finally:
+        _version.repo_dir = ori_repo_dir
+
+
 example_versions = [
     ("1", (1,)),
     ("1.2", (1, 2)),
     ("1.2.3", (1, 2, 3)),
     ("1.2.3", (1, 2, 3)),
-    ("0.29.0.post4#g3175010", (0, 29, 0, "post4", "#g3175010")),
-    ("2.6.0#gcd877db.dirty", (2, 6, 0, "#gcd877db", "dirty")),
-    ("0.15.0.post16#g63b1a427.dirty", (0, 15, 0, "post16", "#g63b1a427", "dirty")),
-    ("1.#foo", (1, "#foo")),
+    ("1.2.3.post9", (1, 2, 3, "post", 9)),
+    ("1.2.3.post10", (1, 2, 3, "post", 10)),
+    ("0.29.0.post4+g3175010", (0, 29, 0, "post", 4)),
+    (
+        "2.6.0+gcd877db.dirty",
+        (
+            2,
+            6,
+            0,
+        ),
+    ),
+    ("0.15.0.post16+g63b1a427.dirty", (0, 15, 0, "post", 16)),
+    ("2.6.1+from_tag_2_6_0.post3.g72c1d22", (2, 6, 1)),
+    ("1.+foo", (1,)),
 ]
 
 
